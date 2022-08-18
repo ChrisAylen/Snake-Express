addb03a904a12b4fa51f82740b63e48529e383fc
Tamin Choudhury <taminc@live.com>
2022-08-18 21:15:12 +0100

    updated models

---
+++ server/models/Score.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const scoreSchema = new Schema({
+  scoreText: {
+    type: Number,
+    trim: true,
+  },
+  scoreAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+});
+
+const Score = model('Score', scoreSchema);
+
+module.exports = Thought;
