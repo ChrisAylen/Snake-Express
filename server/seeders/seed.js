45c49649e013450b7e337033e2490f2b5e91a1af
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-08-20 13:55:49 +0100

    refactored seeding

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,22 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Score } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const scoreSeeds = require('./scoreSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Thought.deleteMany({});
+    await Score.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < scoreSeeds.length; i++) {
+      const { _id, username } = await Score.create(scoreSeeds[i]);
       const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+        { username: username },
         {
           $addToSet: {
-            thoughts: _id,
+            scores: _id,
           },
         }
       );
