cfd664dae6ef7e2511ea7707a1799660d72753b4
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-08-23 14:55:24 +0100

    sign up style

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -5,6 +5,7 @@ import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
+import Navbar from "./Navbar";
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -39,11 +40,17 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div className="card-body">
+    <>
+    <Navbar/>
+    <main className="login h-screen">
+    <br></br>
+    <br></br>
+    <div className="flex mt-20 justify-center items-center">
+        <div className=" container border border-green-600 w-fit content-center  justify-center items-center rounded pt-8 pb-8 pl-6 pr-6">
+      
+        <div className="card ">
+       
+          <div className="card-body ">
             {data ? (
               <p>
                 Success! You may now head{' '}
@@ -51,37 +58,47 @@ const Signup = () => {
               </p>
             ) : (
               <form onSubmit={handleFormSubmit}>
+
+                <h2 className='text-green-800 justify-center items-center text-2xl py-2  px-8 '>Username</h2>
+
                 <input
-                  className="form-input"
+                  className="form-input text-green-800 justify-center items-center"
                   placeholder="Your username"
                   name="username"
                   type="text"
                   value={formState.name}
                   onChange={handleChange}
                 />
+
+                <h2 className='text-green-800 justify-center items-center text-2xl py-2  px-8 '>Email</h2>
                 <input
-                  className="form-input"
+                  className="form-input text-green-800 justify-center items-center "
                   placeholder="Your email"
                   name="email"
                   type="email"
                   value={formState.email}
                   onChange={handleChange}
                 />
+
+                <h2 className='text-green-800 justify-center items-center text-2xl py-2  px-8 '>Password</h2>
                 <input
-                  className="form-input"
+                  className="form-input text-green-800 justify-center items-center"
                   placeholder="******"
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
+                <br></br>
+                <br></br>
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn-primary btn-block bg-green-800 hover:bg-green-500 hover:border-black text-white font-bold py-2 px-4 border border-black rounded flex justify-center items-center"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
                   Submit
                 </button>
+
               </form>
             )}
 
@@ -93,8 +110,11 @@ const Signup = () => {
           </div>
         </div>
       </div>
+    </div>
     </main>
+    </>
   );
 };
+
 
 export default Signup;
